1503f36b97ff48ea25968b600de27ca4354cdaaa
박시율 <kdpark0723@gmail.com>
2019-07-31 00:33:58 +0900

    리팩토링

--- bin/www-cluster.js
+++ bin/www-cluster.js
@@ -11,25 +11,30 @@ const instanceId = uuid.v4();
 const cpuCount = os.cpus().length;
 const workerCount = cpuCount / config.numberOfContainers;
 
+
+function workerMsgListener(msg) {
+  const { workerId } = msg;
+
+  if (msg.cmd === 'MASTER_ID') {
+    cluster.workers[workerId].send({ cmd: 'MASTER_ID', masterId: instanceId });
+  }
+}
+
+function createWorker(count) {
+  for (let i = 0; i < count; i++) {
+    config.logger.log(`Create Worker [${i + 1}/${count}]`);
+    const worker = cluster.fork();
+
+    worker.on('message', workerMsgListener);
+  }
+}
+
 if (cluster.isMaster) {
   config.logger.log(`Server ID : ${instanceId}`);
   config.logger.log(`Server CPU Number : ${cpuCount}`);
   config.logger.log(`Create Worker Number : ${workerCount}`);
 
-  const workerMsgListener = (msg) => {
-    const { workerId } = msg;
-
-    if (msg.cmd === 'MASTER_ID') {
-      cluster.workers[workerId].send({ cmd: 'MASTER_ID', masterId: instanceId });
-    }
-  };
-
-  for (let i = 0; i < workerCount; i++) {
-    config.logger.log(`Create Worker [${i + 1}/${workerCount}]`);
-    const worker = cluster.fork();
-
-    worker.on('message', workerMsgListener);
-  }
+  createWorker(workerCount);
 
   cluster.on('online', (worker) => {
     config.logger.log(`Worker[${worker.process.pid}] online`);
